b026134d555d733a3c4bfe1bbeaaec5d6c15a7d0
mahopson <18131014+Mopson@users.noreply.github.com>
2023-06-27 14:54:33 -0500

    Add function code for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    let i = 1;
 
+    while (arguments[i] != null) {
+        const index = array.indexOf(arguments[i]); //indexOf() uses strict equality (===)
+        if (index > -1) array.splice(index, 1);
+        i++;
+    }
+    return array;
 };
 
 // Do not edit below this line

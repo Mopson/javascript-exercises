4fe2fc69f1ca52876e8b444f91e6aae10a275a01
mahopson <18131014+Mopson@users.noreply.github.com>
2023-06-27 15:28:18 -0500

    Add code for function in exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    
+    //Gauss summation
+
+    //swap values if second argument is smaller than the first, i.e. min becomes max and max becomes min
+    let temp = 0;
+    if(max < min){
+        temp = max;
+        max = min;
+        min = temp;
+    }
+
+    if (Number.isInteger(min) && Number.isInteger(max)) {
+        if (min > 0 && max > 0){
+            return ((max - min) + 1) * (min + max) / 2;
+        }
+        else return "ERROR";
+    }
+    else return "ERROR";
 
 };
 

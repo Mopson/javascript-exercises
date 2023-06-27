e23484951d884ed68c0642b551d3cf2ded831028
mahopson <18131014+Mopson@users.noreply.github.com>
2023-06-27 13:57:59 -0500

    Finish 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (repeatedWord, times) {
+    if (times < 0) return "ERROR";
+    let returnString = '';
+    for (let i = 0; i < times; i++){
+        returnString+=repeatedWord;
+    }
+    return returnString;
 };
 
 // Do not edit below this line

da336c6b4da2d56fdb75c3e058b89e049a409114
mahopson <18131014+Mopson@users.noreply.github.com>
2023-06-28 12:25:27 -0500

    Add code to function in exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+//Math.round(num * 10) / 10 rounds to one decimal place
+
+const convertToCelsius = function(num) {
+  num = (num - 32) * 5/9;
+  return Math.round(num * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = (num * 9/5) + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line

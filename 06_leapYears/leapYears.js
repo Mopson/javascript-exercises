535c382a804124ba2ce44729e1fb80f39fee0888
mahopson <18131014+Mopson@users.noreply.github.com>
2023-06-28 12:15:43 -0500

    Add code for function in exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let isLeap = false;
+    if ((year % 4 === 0) && (year % 400 === 0 || year % 100 !== 0)) {
+        isLeap = true;
+    }
+    return isLeap;
 };
 
 // Do not edit below this line
